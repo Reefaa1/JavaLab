2b605de023b28a687b4e23f3b743f26b3d3305e3
Reefaa1 <fathimarifafrk@gmail.com>
2025-09-25 15:09:14 +0530

    To check a number(zero,positive/negative)

---
+++ Numtype.java
@@ -0,0 +1,15 @@
+//0,pos,or neg
+class Numtype{
+	public static void main(String k[])
+	{
+                    int a=Integer.parseInt(k[0]);
+	 if(a==0)
+		System.out.println(a+" is zero");
+	else if(a>0)
+		System.out.println(a+" is a positive number");
+	else
+		System.out.println(a+" is a negative number");
+}
+}
+		
+			
