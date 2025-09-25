5f7198eba5423718a1e8bb0532229fd8b8e5b26f
Reefaa1 <fathimarifafrk@gmail.com>
2025-09-25 15:12:53 +0530

    To find Sum and Average

---
+++ sumavg.java
@@ -0,0 +1,10 @@
+class sumavg
+{
+	public static void main(String k[])
+	{
+		int a=Integer.parseInt(k[0]);
+		int b=Integer.parseInt(k[1]);
+		int s=a+b;
+		System.out.print("Sum="+s+"\t Average="+s/2);
+	}
+}
