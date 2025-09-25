d499c90e26e895d7d4e33bebe7fb7827a96a8754
Reefaa1 <fathimarifafrk@gmail.com>
2025-09-25 15:14:29 +0530

    To check odd or even

---
+++ oden.java
@@ -0,0 +1,11 @@
+class oden{
+	public static void main(String arg[])
+	{
+		int a=Integer.parseInt(arg[0]);
+		if(a%2==0)
+			System.out.println(a+"is an even number");
+		else
+			System.out.println(a+"is an odd number");
+}
+}
+	
