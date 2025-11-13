6269d850ecec459bb3afd940450ebeebcf4d01fb
Reefaa1 <fathimarifafrk@gmail.com>
2025-11-13 11:24:57 +0530

    find the smallest in given numbers

---
+++ min.java
@@ -0,0 +1,12 @@
+class min
+{
+	public static void main(String arg[])
+	{
+	 int a=Integer.parseInt(arg[0]);
+	 int b=Integer.parseInt(arg[1]);
+	if(a>b)
+		System.out.println(b+" is the smallest number");
+	else
+		System.out.println(a+" is the smallest number");
+}
+}
